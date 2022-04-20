356489af2c1ef6915b1aff512702b63017c6d168
Davy Lucas <davykog2012@gmail.com>
2022-04-20 20:39:12 -0300

    GERANDO O LAYOUT
    Cada linha de movimentação deverá ter a estrutura conforme ilustração seguir:
    2022041600013521779118RAIMUNDO NONATO LOUREIRO CASTE0000127548R0

--- semana2/open-banking/src/main/java/com/mjv/openbanking/GeradorArquivo.java
+++ semana2/open-banking/src/main/java/com/mjv/openbanking/GeradorArquivo.java
@@ -5,11 +5,20 @@ import java.time.LocalDate;
 public class GeradorArquivo {
 
 	public static void main(String[] args) {
-		
-			Movimentacao mov1 = new Movimentacao();
-			mov1.setData(LocalDate.now());
-			
-			System.out.println(mov1.getData());
+
+		Movimentacao movimentacao1 = new Movimentacao();
+		movimentacao1.setData(LocalDate.of(2022, 4, 16));
+		movimentacao1.setCpfCnpj("135.217.791-18");
+		movimentacao1.setNomeCliente("RAIMUNDO NONATO ");// LOUREIRO CASTELO BRANCO
+		movimentacao1.setValor(1275.48);
+		movimentacao1.setTipo(TipoMovimentacao.RECEITA);
+		movimentacao1.setEstornada(false);
+
+		GeradorConteudo gc = new GeradorConteudo();
+		String conteudoGerador = gc.gerar(movimentacao1);
+
+		System.out.println(conteudoGerador);
+
 	}
 
 }
